88a66066280212d50f9de0f57dbbd4af309a6451
Mr.Cow <2292107343@qq.com>
2022-09-25 13:31:50 +0800

    add methods

--- JavaScript/Object/Obj-isEmpty.js
+++ JavaScript/Object/Obj-isEmpty.js
@@ -8,4 +8,6 @@ function isEmpty(obj) {
   const ownSymKeys = Object.getOwnPropertySymbols(obj);
 
   return !ownKeys.length && !ownSymKeys.length;
+  // 也可以写成 return !!Reflect.ownKeys(obj).length;
+  // MDN 文档指出 Reflect.ownKeys 写法就等同于我们上面写的那两个
 }
